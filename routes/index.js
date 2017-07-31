7d7b49e41c91c5c2bb88114b34e743ac2309d6c1
Paul Stovell <paul@paulstovell.com>
2017-07-31 22:27:02 +1000

    Made simple app

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Phoenix: ' + process.env.phoenix_tenant,
+    tenant: process.env.phoenix_tenant,
+    environment: process.env.phoenix_env,
+    machine: process.env.phoenix_machine,
+    deployed: process.env.phoenix_deployed
+  });
+});
+
+module.exports = router;
